f6d9e2005440a892b84144a66a5a5d5006bb639b
paulocinco <jaeggi.paul@gmail.com>
2026-03-18 16:36:39 +0100

    wpmanager

--- src/compas_fab/viewer/__init__.py
+++ src/compas_fab/viewer/__init__.py
@@ -13,10 +13,15 @@ Classes
     :nosignatures:
 
     TrajectoryPlayer
+    WorkpieceManager
 
 """
 from .trajectory_player import TrajectoryPlayer
+# Assuming WorkpieceManager is defined in trajectory_player.py
+# If it's in a different file, change the import accordingly, e.g., from .workpiece_manager import WorkpieceManager
+from .workpiece_manager import WorkpieceManager 
 
 __all__ = [
     "TrajectoryPlayer",
+    "WorkpieceManager",
 ]
